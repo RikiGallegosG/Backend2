c09d5d1862529441f9631e4ac855cb9c661ddc1d
Ricardo Gallegos <75600056+RikiGallegosG@users.noreply.github.com>
2024-11-07 17:50:40 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+app.use(express.static('dist'))
 
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method);
